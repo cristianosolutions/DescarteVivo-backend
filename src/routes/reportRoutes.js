2f0d8db859ecf1a9211c3f046fb05ac3fb1c158b
Cristiano Magno <161599641+cristianosolutions@users.noreply.github.com>
2025-11-28 12:52:57 -0300

    Update reportRoutes.js

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../Old_db");
+const pool = require("../db");
 
 const generatePointsReport = require("../reports/pointsReport");
 const generateUsersReport = require("../reports/usersReport");
